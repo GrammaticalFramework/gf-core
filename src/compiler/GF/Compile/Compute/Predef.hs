cdbe73eb475cf44e1a45b2abacb12756e394016a
John J. Camilleri <john@digitalgrammars.com>
2021-07-08 12:10:41 +0200

    Remove two missing-methods warnings

--- src/compiler/GF/Compile/Compute/Predef.hs
+++ src/compiler/GF/Compile/Compute/Predef.hs
@@ -27,6 +27,10 @@ instance Predef Int where
 
 instance Predef Bool where
   toValue = boolV
+  fromValue v = case v of
+    VCApp (cPredef,cPTrue) [] -> return True
+    VCApp (cPredef,cPFalse) [] -> return False
+    _ -> verror "Bool" v
 
 instance Predef String where
   toValue = string
