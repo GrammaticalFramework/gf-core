GrammaticalFramework/gf-core

commit cdbe73eb475cf44e1a45b2abacb12756e394016a

Remove two missing-methods warnings

src/compiler/GF/Compile/Compute/Predef.hs
@@ -27,6 +27,10 @@ instance Predef Int where
 
 instance Predef Bool where
   toValue = boolV
+  fromValue v = case v of
+    VCApp (cPredef,cPTrue) [] -> return True
+    VCApp (cPredef,cPFalse) [] -> return False
+    _ -> verror "Bool" v
 
 instance Predef String where
   toValue = string

src/runtime/haskell/PGF/Binary.hs
@@ -28,7 +28,7 @@ instance Binary PGF where
            let v = (major,minor)
            if major==pgfMajorVersion && minor<=pgfMinorVersion
              then getPGF'
-             else if v==Old.version 
+             else if v==Old.version
                   then Old.getPGF'
                   else fail $ "Unsupported PGF version "++show (major,minor)
 
@@ -185,6 +185,7 @@ instance Binary Instr where
   put (PUSH_ACCUM (LFlt d)) = putWord8 78 >> put d
   put (POP_ACCUM          ) = putWord8 80
   put (ADD                ) = putWord8 84
+  get = fail "Missing implementation for ‘get’ in the instance declaration for ‘Binary Instr’"
 
 instance Binary Type where
   put (DTyp hypos cat exps) = put (hypos,cat,exps)

commit a1fd3ea142f215d7a030b8a95d32ba0c55dd61fb

Fix bug introduced in cdbe73eb475cf44e1a45b2abacb12756e394016a
Apparently I don't understand how pattern-matching works in Haskell

src/compiler/GF/Compile/Compute/Predef.hs
@@ -28,8 +28,8 @@ instance Predef Int where
 instance Predef Bool where
   toValue = boolV
   fromValue v = case v of
-    VCApp (cPredef,cPTrue) [] -> return True
-    VCApp (cPredef,cPFalse) [] -> return False
+    VCApp (mn,i) [] | mn == cPredef && i == cPTrue -> return True
+    VCApp (mn,i) [] | mn == cPredef && i == cPFalse -> return False
     _ -> verror "Bool" v
 
 instance Predef String where