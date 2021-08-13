08bcd2f0b5b203b55efff4dfe0b13b57b304acc6
krangelov <kr.angelov@gmail.com>
2021-08-13 08:28:30 +0200

    silence warnings

--- src/runtime/haskell/utils.c
+++ src/runtime/haskell/utils.c
@@ -1,19 +1,20 @@
 #include <HsFFI.h>
 #include <pgf/pgf.h>
+#include <stdlib.h>
 
 void hs_free_unmarshaller(PgfUnmarshaller *unmarshaller)
 {
-    hs_free_fun_ptr(unmarshaller->eabs);
-    hs_free_fun_ptr(unmarshaller->eapp);
-    hs_free_fun_ptr(unmarshaller->elit);
-    hs_free_fun_ptr(unmarshaller->emeta);
-    hs_free_fun_ptr(unmarshaller->efun);
-    hs_free_fun_ptr(unmarshaller->evar);
-    hs_free_fun_ptr(unmarshaller->etyped);
-    hs_free_fun_ptr(unmarshaller->eimplarg);
-    hs_free_fun_ptr(unmarshaller->lint);
-    hs_free_fun_ptr(unmarshaller->lflt);
-    hs_free_fun_ptr(unmarshaller->lstr);
-    hs_free_fun_ptr(unmarshaller->dtyp);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->eabs);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->eapp);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->elit);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->emeta);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->efun);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->evar);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->etyped);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->eimplarg);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->lint);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->lflt);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->lstr);
+    hs_free_fun_ptr((HsFunPtr) unmarshaller->dtyp);
     free(unmarshaller);
 }
