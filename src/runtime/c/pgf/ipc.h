204e6456163c4b34fcc7255e058df8d04f9c6b09
krangelov <kr.angelov@gmail.com>
2021-10-25 15:52:32 +0200

    update ipc.h

--- src/runtime/c/pgf/ipc.h
+++ src/runtime/c/pgf/ipc.h
@@ -2,6 +2,9 @@
 #define IPC_H
 
 PGF_INTERNAL_DECL
-pthread_rwlock_t *pgf_acquire_file_rwlock(const char* file_path);
+pthread_rwlock_t *ipc_new_file_rwlock(const char* file_path);
+
+PGF_INTERNAL
+void ipc_release_file_rwlock(const char* file_path);
 
 #endif
