f2572d3bd539a4a64a77c1ab7c727b8ab01a43f1
Krasimir Angelov <kr.angelov@gmail.com>
2022-01-09 14:59:55 +0100

    mark Vector as internal

--- src/runtime/c/pgf/vector.h
+++ src/runtime/c/pgf/vector.h
@@ -2,12 +2,12 @@
 #define VECTOR_H
 
 template <class A>
-struct Vector {
+struct PGF_INTERNAL Vector {
     size_t len;
     A data[];
 };
 
-template <class A> inline
+template <class A> inline PGF_INTERNAL
 ref<Vector<A>> vector_new(size_t len)
 {
     ref<Vector<A>> res = PgfDB::malloc<Vector<A>>(len*sizeof(A));
@@ -15,7 +15,7 @@ ref<Vector<A>> vector_new(size_t len)
     return res;
 }
 
-template <class C, class A> inline
+template <class C, class A> inline PGF_INTERNAL
 ref<C> vector_new(Vector<A> C::* field, size_t len)
 {
     ptrdiff_t offset = (ptrdiff_t) &(((C*) NULL)->*field);
@@ -24,13 +24,13 @@ ref<C> vector_new(Vector<A> C::* field, size_t len)
     return res;
 }
 
-template <class A> inline
+template <class A> inline PGF_INTERNAL
 ref<A> vector_elem(ref<Vector<A>> v, size_t index)
 {
     return ref<A>::from_ptr(&v->data[index]);
 }
 
-template <class A> inline
+template <class A> inline PGF_INTERNAL
 A *vector_elem(Vector<A> *v, size_t index)
 {
     return &v->data[index];
