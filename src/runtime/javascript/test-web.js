0bac8f0daebc04c9e8b6170a022f6dea01125e2e
John J. Camilleri <john@digitalgrammars.com>
2022-08-01 14:47:28 +0200

    Load and read PGF in test-web: doesn't fail but abstract name is empty

--- src/runtime/javascript/test-web.js
+++ src/runtime/javascript/test-web.js
@@ -1,6 +1,19 @@
 Module.onRuntimeInitialized = () => {
     const JSPGF = mkAPI(Module);
 
+    fetch('Foods.pgf')
+      .then((response) => response.arrayBuffer())
+      .then((data) => {
+        const pgfPathFS = '/tmp/Foods.pgf';
+        Module.FS.writeFile(pgfPathFS, new DataView(data));
+
+        // Read PGF
+        const pgf = JSPGF.readPGF(pgfPathFS);
+
+        // Print its name
+        console.log(JSPGF.abstractName(pgf));
+      })
+    
     // Parse expression
     const expr = JSPGF.readExpr("Pred (Another (x f))");
 
