7f7f543f592cfab8b8031a5f90255c6e5a8d0e51
Andreas Källberg <anka.213@gmail.com>
2023-09-26 10:09:23 +0200

    Fix quasiquotation syntax error

--- src/compiler/GF/CompileInParallel.hs
+++ src/compiler/GF/CompileInParallel.hs
@@ -61,7 +61,7 @@ parallelBatchCompile jobs opts rootfiles0 =
 
         usesPresent (_,paths) = take 1 libs==["present"]
           where
-            libs = [p|path<-paths,
+            libs = [p | path<-paths,
                       let (d,p0) = splitAt n path
                           p = dropSlash p0,
                       d==lib_dir,p `elem` all_modes]
@@ -175,7 +175,7 @@ batchCompile1 lib_dir (opts,filepaths) =
                    " from being compiled."
        else return (maximum ts,(cnc,gr))
 
-splitEither es = ([x|Left x<-es],[y|Right y<-es])
+splitEither es = ([x | Left x<-es],[y | Right y<-es])
 
 canonical path = liftIO $ D.canonicalizePath path `catch` const (return path)
 
